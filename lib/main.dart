2e11d834977cd0512867ba3b11a4994f04ad2deb
mutohiroshi <mutohiroshi@team-lab.com>
2020-07-11 15:53:52 +0900

    feat: add TextField and Add Button

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,23 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(decoration: InputDecoration(labelText: 'Title'),),
+                  TextField(decoration: InputDecoration(labelText: 'Amount'),),
+                  FlatButton(
+                    child: Text('Add Transaction'),
+                    textColor: Colors.purple,
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ),
           Column(
             children: transactions.map((tx) {
               return Card(

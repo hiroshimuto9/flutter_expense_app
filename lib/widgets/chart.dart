be7ee6f2c0707c53f0605e029430d153afcef3f5
mutohiroshi <mutohiroshi@team-lab.com>
2020-07-19 18:55:46 +0900

    feat: create calculation func for chart_bar

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './chart_bar.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
@@ -24,9 +25,15 @@ class Chart extends StatelessWidget {
       }
 
       return {
-        'day': DateFormat().format(weekDay).substring(0,1),
+        'day': DateFormat.E().format(weekDay).substring(0,1),
         'amount': totalSum
       };
+    });
+  }
+
+  double get totalSpending {
+    return groupTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -37,7 +44,11 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupTransactionValues.map((data) {
-          return Text('${data['day']}: ${data['amount']}');
+          return ChartBar(
+            data['day'],
+            data['amount'],
+            totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending,
+          );
         }).toList(),
       ),
     );
--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -20,7 +20,8 @@ class ChartBar extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                 border: Border.all(
-                  color: Colors.grey, width: 1.0
+                  color: Colors.grey,
+                  width: 1.0
                 ),
                 color: Color.fromRGBO(220, 220, 220, 1),
                 borderRadius: BorderRadius.circular(20),

bf7cc90dc54ee92daa57fa8178c944e1fca23189
mutohiroshi <mutohiroshi@team-lab.com>
2020-07-25 18:26:04 +0900

    delete: delete unused code

--- lib/main.dart
+++ lib/main.dart
@@ -44,27 +44,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  // String titleInput;
-  // String amountInput;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransactions = [
-    // Transaction(
-    //   id: 't1',
-    //   title: 'New Shoes',
-    //   amount: 69.99, date:
-    //   DateTime.now()
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Weekly Groceries',
-    //   amount: 15.99, date:
-    //   DateTime.now()
-    // ),
-  ];
+  final List<Transaction> _userTransactions = [];
 
   List<Transaction> get _recentTransactions {
     return  _userTransactions.where((tx) {
